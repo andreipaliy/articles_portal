5a5c6031638333c75294db444fcbde695165f740
Andrei Palii <andreipaliy10@gmail.com>
2023-06-06 18:57:05 +0300

    Set theme
    Add files with variables for two themes + global scss file with common properties.
    Set theme to be hold in React context + create Provider + hook useTheme which set theme in localStorare and provide an API to acces theme and to toggle it's value

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
-import './index.scss'
+import '../styles/index.scss'
 import { Link } from 'react-router-dom'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { Suspense } from 'react'
+import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from '../pages/MainPage/MainPage.async'
+import { Suspense} from 'react'
+import { useTheme } from '../theme/useTheme'
+
+
 
 export const App = () => {
-  return (
-    <div className="app"> 
+const {theme, toggleTheme} = useTheme()
+
+return (
+    <div className={`app ${theme}`}> 
+    <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
         <Suspense fallback={<div>Loading...</div>}>
@@ -16,6 +22,6 @@ export const App = () => {
         <Route path={'/'} element={<MainPageAsync />}/>
       </Routes>
         </Suspense>
-    </div>
+    </div> 
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import { render } from "react-dom";
 import { App } from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./theme/ThemeProvider";
 
 render(
 <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
 </BrowserRouter>, document.getElementById('root'))
