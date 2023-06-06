91a64e78f3c504baa41b22685fec21a77a0a551f
Andrei Palii <andreipaliy10@gmail.com>
2023-06-06 17:32:37 +0300

    Add routing + async chunk splitting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from './Counter'
+import { Route, Routes } from 'react-router-dom'
 import './index.scss'
+import { Link } from 'react-router-dom'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { Suspense } from 'react'
 
 export const App = () => {
   return (
-    <div className="app">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto id sapiente ea maxime vero molestias minima maiores dolorum officiis eaque porro quasi ex reiciendis et, nostrum quis temporibus eligendi. Neque!
-        <Counter /></div>
+    <div className="app"> 
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path={'/about'} element={<AboutPageAsync />}/> 
+        <Route path={'/'} element={<MainPageAsync />}/>
+      </Routes>
+        </Suspense>
+    </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
 import { render } from "react-dom";
 import { App } from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById('root'))
+render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>, document.getElementById('root'))
