eb25a4990bba56da0ae96857eafd5bd155890ad3
Andrei Palii <andreipaliy10@gmail.com>
2023-06-15 16:32:52 +0300

    Add Not Found 404 page + Loading page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,17 +1,16 @@
 import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig'
+import { PageLoader } from 'widgets/PageLoader'
 
-export const AppRouter = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className='page-wrapper'>
-        <Routes>
-          {Object.values(routeConfig).map(({ element, path }) => (
-            <Route path={path} element={element} key={path} />
-          ))}
-        </Routes>
-      </div>
+export const AppRouter = () => (
+    <Suspense fallback={<PageLoader />}>
+        <div className='page-wrapper'>
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route path={path} element={element} key={path} />
+                ))}
+            </Routes>
+        </div>
     </Suspense>
-  )
-}
+)
---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,19 @@
+import { classNames } from 'shared/lib/classNames'
+import { useTranslation } from 'react-i18next'
+import cls from './NotFoundPage.module.scss'
+
+interface NotFoundPageProps {
+  className?: string
+}
+
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+    const [t] = useTranslation()
+
+    return (
+        <p
+            className={classNames(cls.NotFoundPage, {}, [className])}
+        >
+            {t('Page not found')}
+        </p>
+    )
+}
