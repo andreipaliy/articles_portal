7dfe88fca86b3273d9463675424782c02cec78b7
Andrei Palii <andreipaliy10@gmail.com>
2023-06-21 16:56:47 +0300

    Remove unused copy of ThemeDecorator

--- src/shared/ui/AppLink/ThemeDecorator.tsx
+++
@@ -1,6 +0,0 @@
-import { Theme } from 'app/providers/ThemeProvider'
-import { Story } from '@storybook/blocks'
-import 'app/styles/index.scss'
-
-//TODO: fix eslint formating ThemeDecorator in one line on Save
-export const ThemeDecorator = (theme: Theme) => (StoryComponent:typeof Story) => (<div className={`app ${theme}`}><StoryComponent /></div>)
