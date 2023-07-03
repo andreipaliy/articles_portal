ef641659aebb9c8bed31d733cb80e3a08d1627f9
Andrei Paliy <98047265+andreipaliy@users.noreply.github.com>
2023-07-03 22:26:28 +0300

    Update workflow file

--- src/shared/config/storybook/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator.tsx
@@ -1,11 +1,11 @@
-import { Theme } from 'app/providers/ThemeProvider'
+import { Theme, ThemeProvider } from 'app/providers/ThemeProvider'
 import { Story } from '@storybook/blocks'
 import 'app/styles/index.scss'
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent:typeof Story) => (
-    // <ThemeProvider initialTheme={theme}>
-    <div className={`app ${theme}`}>
-        <StoryComponent />
-    </div>
-    // </ThemeProvider>
+    <ThemeProvider initialTheme={theme}>
+        <div className={`app ${theme}`}>
+            <StoryComponent />
+        </div>
+    </ThemeProvider>
 )
