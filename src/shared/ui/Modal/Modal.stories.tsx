ca04e2fadb727407e24c400c7db8483824bd31fb
Andrei Paliy <98047265+andreipaliy@users.noreply.github.com>
2023-07-03 21:57:21 +0300

    Change content of the modal to check if sensibility causes the issue

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -16,6 +16,6 @@ const Template: StoryFn<ModalProps> = (args) => <Modal {...args} />
 // TODO: to write styling of modal based on current theme + write story for it
 export const Primary = Template.bind({})
 Primary.args = {
-    children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsam. Corrupti, quisquam eos. Esse porro corporis quasi illum nobis! Corrupti placeat aspernatur deleniti reprehenderit, esse cupiditate eveniet nisi neque. Nobis Exercitationem eius saepe sint earum, numquam unde autem amet placeat ex repellat quod magnam fuga, natus molestiae dicta mollitia aliquam voluptates quidem laudantium reprehenderit quos perferendis. Illum quos laudantium debitis. Cupiditate vitae molestiae similique aspernatur libero voluptate, labore rerum inventore, obcaecati mollitia adipisci minus, ratione qui dignissimos hic! Magni voluptatibus, facilis ipsa quidem ut ab molestias amet possimus eveniet harum?',
+    children: 'Lorem ipsum',
     isOpen: true,
 }
